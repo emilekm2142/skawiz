57d69a7d11a1fa6ccf125d3f5b8d35dd0c154739
emilekm2142 <emilm7843@gmail.com>
2020-04-18 17:11:02 +0300

    joy

--- gui.py
+++ gui.py
@@ -1,38 +1,67 @@
 from tkinter import *
+from datetime import datetime, timedelta
 from _thread import start_new_thread
-points = []
+from typing import Tuple, List
+import string
+import colorsys
+import copy
+from statistics import mean, StatisticsError
+sensor_points = {}
+
 config={
-    "screenX":300,
-    "screenY":500,
-    "screenTitle":"SKA"
+    "screenX":1280,
+    "screenY":1000,
+    "screenTitle":"Tellus"
 }
 window = Tk()
 window.title(config["screenTitle"])
 canvas = Canvas(window, width=config["screenX"],height=config["screenY"],background="white")
 canvas.pack()
-def draw_point(new_point:tuple):
-    points.append(canvas.create_rectangle(new_point[0],new_point[1],new_point[0]+1,new_point[1]+1,fill="black"))
-def get_coordinates(p):
+textField = canvas.create_text(100,10,text="Time")
+textField2 = canvas.create_text(40,60,text="Latest reading", anchor=SW)
+def mapFromTo(x,a,b,c,d):
+    y=(x-a)/(b-a)*(d-c)+c
+    return y
+
+def update_sensor_point(id,  xy, value, timestamp):
+    global sensor_points
+    canvas.itemconfigure(textField, text = str(timestamp))
+    
     try:
-        return (int(canvas.coords(p)[0]),int(canvas.coords(p)[1]))
-    except IndexError:
-        return None
-def find_point(point):
-    try:
-        for p in points:
-            if int(canvas.coords(p)[0]) == c[0] and int(canvas.coords(p)[1]) == c[1]:
-                return p
-    except:
+        canvas.itemconfigure(textField2, text = "Latest reading: "+ str(value)+"*C.\n Current average: "+str(round(mean([x[2] for x in sensor_points.values()]),2)))
+    except StatisticsError:
         pass
-def delete_point(c:tuple):
-    try:
-        for p in points:
-            if int(canvas.coords(p)[0]) == c[0] and int(canvas.coords(p)[1]) == c[1]:
-                canvas.delete(p)
-                points.remove(p)
-                break
-       # point = [p for p in points if int(canvas.coords(p)[0]) == c[0] and int(canvas.coords(p)[1]) == c[1]]
-        #print(point)
-        #canvas.delete(point[0])
-    except IndexError:
-        pass
+    min_hsl = (228/360, 1, .50)
+    max_hsl = (360/360, 1, .50)
+    progress = mapFromTo(value, 18,25,min_hsl[0],max_hsl[0])
+    #print(progress)
+    color = colorsys.hls_to_rgb(progress,0.5,1)
+    #print(color)
+    if id not in sensor_points:
+        sensor_points[id] = [canvas.create_oval(xy[0]-8,xy[1]-8,xy[0]+8,xy[1]+8, fill='#%02x%02x%02x' % (int(color[0]*255),int(color[1]*255),int(color[2]*255))), timestamp, value, canvas.create_text(xy[0]+20,xy[1]+3,text=value)]
+    else:
+        canvas.itemconfig(sensor_points[id][0], fill='#%02x%02x%02x' % (int(color[0]*255),int(color[1]*255),int(color[2]*255)) )
+        sensor_points[id][1]=timestamp
+        sensor_points[id][2]=value
+        canvas.itemconfigure(sensor_points[id][3], text = str(value))
+    new_dict = sensor_points
+    copied=False
+    for key,value in sensor_points.items():
+        
+        if value[1]+timedelta(minutes = 60) < timestamp:
+            if not copied:
+                copied=True
+                new_dict = copy.deepcopy(sensor_points)
+            canvas.delete(sensor_points[id][0])
+            canvas.delete(sensor_points[id][3])
+            del new_dict[key]
+    if copied: sensor_points=new_dict
+    
+    #print(sensor_points)
+def updateState(points):
+   
+    for point in points:
+       # print(point)
+        textField = canvas.create_text(point[0][0]+16,point[0][1]-8,text=point[1])
+       
+        canvas.create_oval(point[0][0],point[0][1],point[0][0]+1,point[0][1]+1,fill="black")
--- main.py
+++ main.py
@@ -1,30 +1,70 @@
 import gui
+from scipy.interpolate import interp1d
 from gui import config
 from getData import get_data,vary_point
 from time import sleep
+import pandas as pd
+from dateutil import parser
+from datetime import datetime, timedelta
 from _thread import start_new_thread
+data_file = open("data.csv", 'r')
+current_time = parser.parse("2017-06-26 10:26:02.779")
+
+
+
 translate_to_screen = lambda distance: int(distance)
 center = config["screenX"]/2
-def main():
-    points = get_data()
-    for point in points:
-        gui.draw_point((center,translate_to_screen(point)))
+leftTopCorner = (65.05860348, 25.46612754)
+leftBottomCorner = (65.05860430 ,25.46470780)
 
-def vary():
-    while True:
-        print("step")
-        new_points=[]
-        for point in gui.points:
-            p = gui.get_coordinates(point)
-            new_points.append(vary_point(p))
-            gui.delete_point(p)
-            #print("new point: ",new)
-        for point in new_points:
-            gui.draw_point(point)
-        #gui.points=new_points
-            
-        sleep(2)
+rightTopCorner = (65.05892835,25.46612620)
+rightBottomCorner = (65.05892835,25.46471000)
+corners = [leftTopCorner, leftBottomCorner, rightTopCorner, rightBottomCorner]
+def load_sensors_data(filename):
+    handle = open(filename, 'r')
+    d={}
+    for line in handle:
+        k = line.split("\t")
+        d[k[1]] = (float(k[3].replace(",",'.')), float(k[4].replace(",",'.')), k[2].split("-")[0])
+    handle.close()
+    return d
+sensors = load_sensors_data("sensors.csv")
+
+def mapFromTo(x,a,b,c,d):
+    y=(x-a)/(b-a)*(d-c)+c
+    return y
+mapToScreen = lambda x: (mapFromTo(x[0], min([d[0] for d in corners]),max([d[0] for d in corners]),0,1) * (720+200), mapFromTo(x[1], min([d[1] for d in corners]),max([d[1] for d in corners]),0,1)*(1000) )
+
+def main():
+    gui.updateState([(mapToScreen(x),x[2]) for x in sensors.values()])
+
+
+line_range = [0,400]
+def get_temperature_for_sensor(key,current_time):
+    pass
+def main_loop():
+    global current_time
+    i=0
+    for line in data_file:
+        i+=1 
+        if i<500000: continue
+        if "timestamp,co2,humidity" in line : continue
+       
+        values = line.split(",")
+        time= parser.parse(values[-6])
+        tmp = values[-2]
+        sensor_id = values[0].replace("-", '').upper()
+       
+        try:
+            gui.update_sensor_point(sensor_id, mapToScreen(sensors[sensor_id]), float(tmp),time)
+        except KeyError:
+            pass
+            #print("no such sensor: ", sensor_id)
+    # while True:
+    #     current_time+=timedelta(seconds=60)
+    #     gui.updateState([mapToScreen(value)+get_temperature_for_sensor(key, current_time) for key,value in sensors.items()])
+    #     sleep(0.2)
 
 gui.window.after(0, main)
-start_new_thread(vary,())
+start_new_thread(main_loop,())
 gui.window.mainloop()
